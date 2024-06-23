1513643d0b8ed19afb8f0c222c2de6c7d2e70bea
graphieros <alec.lloyd.probert@gmail.com>
2024-06-23 11:10:48 +0200

    Updated example

--- src/App.vue
+++ src/App.vue
@@ -6,38 +6,78 @@ const types = ref([
 {
     color: '#BBCCCC',
     description: 'door',
-    icon: 'D',
-    typeId: 1
+    initial: 'D',
+    typeId: 1,
+    icon: 'door',
+    iconColor: '#1A1A1A'
   },
   {
     color: '#8A8A8A',
     description: 'wall',
-    icon: 'W',
+    initial: 'W',
     typeId: 2,
+    icon: 'wall',
+    iconColor: '#1A1A1A'
   },
   {
     color: '#3366DD',
     description: 'server',
-    icon: 'C',
-    typeId: 3
+    initial: 'C',
+    typeId: 3,
+    icon: 'server',
+    iconColor: '#FFFFFF'
   },
   {
     color: "#DD6633",
     description: "power",
-    icon: 'P',
-    typeId: 4
+    initial: 'P',
+    typeId: 4,
+    icon: 'bolt',
+    iconColor: 'yellow'
   },
   {
     color: "#71a4a8",
     description: "monitor",
-    icon: 'M',
-    typeId: 5
+    initial: 'M',
+    typeId: 5,
+    icon: 'deviceLaptop',
+    iconColor: "white"
+  },
+  {
+    color: '#1A1A1A',
+    description: "router",
+    initial: 'R',
+    typeId: 6,
+    icon: 'router',
+    iconColor: '#FFFFFF'
+  },
+  {
+    color: '#7aebab',
+    description: "database",
+    initial: 'D',
+    typeId: 7,
+    icon: 'database',
+    iconColor: '#1A1A1A'
+  },
+  {
+    color: '#f595e5',
+    description: "phone",
+    initial: 'P',
+    typeId: 8,
+    icon: 'phone',
+    iconColor: '#1A1A1A'
+  },
+  {
+    color: '#95d5f5',
+    description: "AC",
+    initial: 'P',
+    typeId: 8,
+    icon: 'airConditioning',
+    iconColor: '#1A1A1A'
   },
 ])
 
 const items = ref([
-  {x: 2, y: 2, h: 1, w: 1, typeId: 3},
-  {x: 2, y: 5, h: 3, w: 5, typeId: 4},
 ])
 
 const config = ref({
@@ -47,11 +87,11 @@ const config = ref({
   crosshairBackground: "#4A4A4A",
   fontFamily: "Arial",
   gridFill: "#3A3A3A",
-  gridHeight: 42,
+  gridHeight: 20,
   gridHighlightColor: "#00FF00",
   gridStroke: "#1A1A1A",
   gridStrokeWidth: 0.02,
-  gridWidth: 12,
+  gridWidth: 20,
   handleFill: "#FFFFFF",
   handleSize: 0.3,
   iconColor: "#1A1A1A",
@@ -67,11 +107,6 @@ const plan = ref(null)
 function getItems() {
   console.log(plan.value.getItems())
 }
-
-// @change="change"
-// @delete="deleteItem"
-// @select="selectItem"
-// @unselect="unselectItem"
 
 function change(item) {
   console.log('CHANGE', item)
@@ -154,43 +189,13 @@ function createdItem(item) {
       </template>
 
       <template #componentIcon="{ placedItem, maxSize }">
-        <svg xmlns="http://www.w3.org/2000/svg" v-if="placedItem.description === 'server'" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFFFFF" fill="none" stroke-linecap="round" stroke-linejoin="round" :style="`width:${maxSize}px; position: absolute; transform: scale(0.8, 0.8)`">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-        <path d="M3 4m0 3a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v2a3 3 0 0 1 -3 3h-12a3 3 0 0 1 -3 -3z" />
-        <path d="M3 12m0 3a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v2a3 3 0 0 1 -3 3h-12a3 3 0 0 1 -3 -3z" />
-        <path d="M7 8l0 .01" />
-        <path d="M7 16l0 .01" />
-        <path d="M11 8h6" />
-        <path d="M11 16h6" />
-    </svg>
 
-      <svg xmlns="http://www.w3.org/2000/svg" v-if="placedItem.description === 'wall'" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFFFFF" fill="none" stroke-linecap="round" stroke-linejoin="round" :style="`width:${maxSize}px; position: absolute; transform: scale(0.8, 0.8)`">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-        <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
-        <path d="M4 8h16" />
-        <path d="M20 12h-16" />
-        <path d="M4 16h16" />
-        <path d="M9 4v4" />
-        <path d="M14 8v4" />
-        <path d="M8 12v4" />
-        <path d="M16 12v4" />
-        <path d="M11 16v4" />
-    </svg>
-
-      <svg xmlns="http://www.w3.org/2000/svg" v-if="placedItem.description === 'door'" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFFFFF" fill="none" stroke-linecap="round" stroke-linejoin="round" :style="`width:${maxSize}px; position: absolute; transform: scale(0.8, 0.8)`">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-        <path d="M14 10m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
-        <path d="M21 12a9 9 0 1 1 -18 0a9 9 0 0 1 18 0z" />
-        <path d="M12.5 11.5l-4 4l1.5 1.5" />
-        <path d="M12 15l-1.5 -1.5" />
-    </svg>
-
-      <svg xmlns="http://www.w3.org/2000/svg" v-if="placedItem.description === 'power'" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFFFFF" fill="none" stroke-linecap="round" stroke-linejoin="round" :style="`width:${maxSize}px; position: absolute; transform: scale(0.8, 0.8)`">
+      <svg xmlns="http://www.w3.org/2000/svg" v-if="placedItem.description === 'power'" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFFFFF" fill="none" stroke-linecap="round" stroke-linejoin="round" :style="`width:${maxSize}px; scale(0.8, 0.8);`">
         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
         <path d="M13 3l0 7l6 0l-8 11l0 -7l-6 0l8 -11" />
     </svg>
 
-      <svg xmlns="http://www.w3.org/2000/svg" v-if="placedItem.description === 'monitor'" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFFFFF" fill="none" stroke-linecap="round" stroke-linejoin="round" :style="`width:${maxSize}px; position: absolute; transform: scale(0.8, 0.8)`">
+      <svg xmlns="http://www.w3.org/2000/svg" v-if="placedItem.description === 'monitor'" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFFFFF" fill="none" stroke-linecap="round" stroke-linejoin="round" :style="`width:${maxSize}px; scale(0.8, 0.8);`">
         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
   <path d="M3 19l18 0" />
   <path d="M5 6m0 1a1 1 0 0 1 1 -1h12a1 1 0 0 1 1 1v8a1 1 0 0 1 -1 1h-12a1 1 0 0 1 -1 -1z" />
